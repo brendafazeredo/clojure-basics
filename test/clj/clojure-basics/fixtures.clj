a85016f7a3461c0a515544db979b40a2d0a78b87
Brenda Azeredo <brendaazeredo@Brendas-Air.bbrouter>
2024-04-01 10:31:01 -0300

    First try of todos.

--- test/clj/clojure-basics/fixtures.clj
+++ test/clj/clojure-basics/fixtures.clj
@@ -9,10 +9,29 @@
 (defn start-db []
   (d/create-database conn-name)
   (let [conn (d/connect conn-name)]
-    (reset! db/conn  (dm/fork-conn conn))
+    (reset! db/conn (dm/fork-conn conn))
     (db/load-schema)))
 
 (defn with-db [f]
   (with-redefs [db/conn (atom nil)]
     (start-db)
     (f)))
+
+(defn create-todo [todo-name]
+  (let [tx-result @(d/transact @db/conn [{:db/id (d/tempid :db.part/user)
+                                           :todo/name todo-name}])]
+    (first (:tempids tx-result))))
+
+(defn read-todo [todo-id]
+  (d/q '[:find ?name
+         :where [?todo :todo/name ?name]
+                [?todo :db/id ?id]]
+       @db/conn
+       todo-id))
+
+(defn update-todo [todo-id new-name]
+  (d/transact @db/conn [{:db/id todo-id
+                         :todo/name new-name}]))
+
+(defn delete-todo [todo-id]
+  (d/transact @db/conn [[:db/retractEntity todo-id]]))
--- test/clj/clojure-basics/todo_test.clj
+++ test/clj/clojure-basics/todo_test.clj
@@ -3,19 +3,33 @@
    [clojure-basics.todos.model :as model]
    [clojure-basics.db :as db]
    [clojure.test :refer [deftest is run-tests testing use-fixtures]]
-   [fixtures :as fixtures :refer [with-db]]
+   [clojure-basics.fixtures :as fixtures :refer [with-db create-todo read-todo update-todo delete-todo]]
    ))
 
 (use-fixtures
   :once
   with-db)
 
-(deftest db-connection
-  (testing "db connection"
-    (let [todo-name "test"
-          todo (model/create-todo todo-name)]
-      (is (= todo-name
-             (:todo/name todo))))))
+(deftest create-todo-test
+  (testing "Create Todo"
+    (let [todo-name "Test Todo"
+          todo-id (create-todo todo-name)
+          todo (read-todo todo-id)]
+      (is (= todo-name (first todo))))))
 
+(deftest update-todo-test
+  (testing "Update Todo"
+    (let [todo-name "Test Todo"
+          new-name "Updated Todo"
+          todo-id (create-todo todo-name)]
+      (update-todo todo-id new-name)
+      (is (= new-name (-> (read-todo todo-id) first))))))
+
+(deftest delete-todo-test
+  (testing "Delete Todo"
+    (let [todo-name "Test Todo"
+          todo-id (create-todo todo-name)]
+      (delete-todo todo-id)
+      (is (nil? (read-todo todo-id))))))
 
 (run-tests)
